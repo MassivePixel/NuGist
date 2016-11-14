ec877d387272c88074ffee722c60970fa059dcad
Toni Petrina <toni@toni.com>
2016-11-14 15:14:18 +0100

    Added Monaco editor

---
+++ NuGist.Nuget/Commands.cs
@@ -0,0 +1,102 @@
+﻿using NuGet.Commands;
+using NuGet.Common;
+using NuGet.Packaging;
+using NuGet.Versioning;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace NuGist.Nuget
+{
+    class ConsoleLogger : ILogger
+    {
+        public void LogDebug(string data) => Console.WriteLine($"Debug: {data}");
+
+        public void LogError(string data) => Console.WriteLine($"Error: {data}");
+
+        public void LogErrorSummary(string data) => Console.WriteLine($"Error summary: {data}");
+
+        public void LogInformation(string data) => Console.WriteLine($"Information: {data}");
+
+        public void LogInformationSummary(string data) => Console.WriteLine($"Information summary: {data}");
+
+        public void LogMinimal(string data) => Console.WriteLine($"Minimal: {data}");
+
+        public void LogVerbose(string data) => Console.WriteLine($"Verbose: {data}");
+
+        public void LogWarning(string data) => Console.WriteLine($"Warning: {data}");
+    }
+
+    public class PackParams
+    {
+        public string NuspecFileName { get; set; }
+        public string Version { get; set; }
+        public Dictionary<string, string> Properties = new Dictionary<string, string>();
+    }
+
+    public class Commands
+    {
+        static string MinClientVersion = "3.0.0";
+        static Version _minClientVersionValue;
+
+        public void Pack(string root, PackParams p)
+        {
+            var packArgs = new PackArgs();
+            packArgs.Logger = new ConsoleLogger();
+            packArgs.Arguments = new string[] { Path.Combine(root, "input", p.NuspecFileName), "-Version", p.Version };
+            packArgs.OutputDirectory = Path.Combine(root, "output");
+            packArgs.BasePath = Path.Combine(root, "input");
+
+            // The directory that contains msbuild
+            packArgs.MsBuildDirectory = null;// new Lazy<string>(() => @"C:\Program Files (x86)\MSBuild\14.0\Bin");
+
+            // Get the input file
+            packArgs.Path = PackCommandRunner.GetInputFile(packArgs);
+
+            // Set the current directory if the files being packed are in a different directory
+            PackCommandRunner.SetupCurrentDirectory(packArgs);
+
+            Console.WriteLine(packArgs.Path);
+
+            if (!string.IsNullOrEmpty(MinClientVersion))
+            {
+                if (!Version.TryParse(MinClientVersion, out _minClientVersionValue))
+                {
+                    throw new Exception("invalid version");
+                }
+            }
+
+            packArgs.Build = false;
+            packArgs.Exclude = new string[0];
+            packArgs.ExcludeEmptyDirectories = true;
+            packArgs.IncludeReferencedProjects = false;
+
+            packArgs.LogLevel = LogLevel.Verbose;
+
+            packArgs.MinClientVersion = _minClientVersionValue;
+            packArgs.NoDefaultExcludes = true;
+            packArgs.NoPackageAnalysis = true;
+            if (p.Properties.Any())
+            {
+                packArgs.Properties.AddRange(p.Properties);
+            }
+            packArgs.Suffix = string.Empty;
+            packArgs.Symbols = false;
+            packArgs.Tool = false;
+
+            if (!string.IsNullOrEmpty(p.Version))
+            {
+                NuGetVersion version;
+                if (!NuGetVersion.TryParse(p.Version, out version))
+                {
+                    throw new Exception();
+                }
+                packArgs.Version = version.ToNormalizedString();
+            }
+
+            var packCommandRunner = new PackCommandRunner(packArgs, null);
+            packCommandRunner.BuildPackage();
+        }
+    }
+}
---
+++ NuGist.Web/Controllers/HomeController.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Hosting;
+using NuGist.Nuget;
+using System.IO;
+using System.Net.Mime;
+
+namespace NuGist.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IHostingEnvironment _hostingEnvironment;
+
+        public HomeController(IHostingEnvironment hostingEnvironment)
+        {
+            this._hostingEnvironment = hostingEnvironment;
+        }
+
+        public IActionResult Index() => View();
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+            return View();
+        }
+
+        public IActionResult Error() => View();
+
+        public string Root => Path.Combine(_hostingEnvironment.ContentRootPath, "temp");
+
+        public ActionResult Build()
+        {
+            Directory.CreateDirectory(Root);
+            var commands = new Commands();
+            var sub = Guid.NewGuid().ToString();
+            var dir = Path.Combine(Root, sub);
+
+            var slug = "namespace.package";
+            var filename = "package.cs";
+
+            Directory.CreateDirectory(Path.Combine(dir, "input"));
+            Directory.CreateDirectory(Path.Combine(dir, "output"));
+
+            System.IO.File.WriteAllText(Path.Combine(dir, "input", filename), "hello");
+            System.IO.File.WriteAllText(Path.Combine(dir, "input", slug + ".nuspec"),
+$@"<?xml version=""1.0""?>
+<package >
+  <metadata>
+    <id>test.cs</id>
+    <version>1.0.0</version>
+    <authors>ToniP</authors>
+    <owners>ToniP</owners>
+    <licenseUrl>http://LICENSE_URL_HERE_OR_DELETE_THIS_LINE</licenseUrl>
+    <projectUrl>http://PROJECT_URL_HERE_OR_DELETE_THIS_LINE</projectUrl>
+    <iconUrl>http://ICON_URL_HERE_OR_DELETE_THIS_LINE</iconUrl>
+    <requireLicenseAcceptance>false</requireLicenseAcceptance>
+    <description>Package description</description>
+    <releaseNotes>Summary of changes made in this release of the package.</releaseNotes>
+    <copyright>Copyright 2016</copyright>
+    <tags>Tag1 Tag2</tags>
+  </metadata>
+  <files>
+    <file src=""{filename}"" target=""content"" />
+  </files>
+</package>");
+
+            commands.Pack(dir, new PackParams
+            {
+                NuspecFileName = slug + ".nuspec",
+                Version = "0.1.0-alpha"
+            });
+            return View();
+        }
+
+        public ActionResult List()
+        {
+            Directory.CreateDirectory(Root);
+            var directories = Directory.GetDirectories(Root);
+
+            return Json(new
+            {
+                directories
+            });
+        }
+
+        public ActionResult GetPackage(string id)
+        {
+            var dir = Path.Combine(Root, id, "output");
+            var file = Directory.GetFiles(dir)[0];
+            //using (var stream = System.IO.File.OpenRead(file))
+                //return File(stream, "application/x-compressed");
+                return File(System.IO.File.ReadAllBytes(file), MediaTypeNames.Text.Plain, Path.GetFileName(file));
+        }
+    }
+}
