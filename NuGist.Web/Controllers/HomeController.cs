4fd09be17cb072510ef7a4f4d4948e07c86a68d2
Toni Petrina <toni@toni.com>
2016-11-14 15:30:09 +0100

    Removed unused files

--- NuGist.Web/Controllers/HomeController.cs
+++ NuGist.Web/Controllers/HomeController.cs
@@ -18,18 +18,6 @@ namespace NuGist.Web.Controllers
 
         public IActionResult Index() => View();
 
-        public IActionResult About()
-        {
-            ViewData["Message"] = "Your application description page.";
-            return View();
-        }
-
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
-            return View();
-        }
-
         public IActionResult Error() => View();
 
         public string Root => Path.Combine(_hostingEnvironment.ContentRootPath, "temp");
