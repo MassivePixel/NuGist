85eea9582ddf04f4e82313a28374877aff28bc7f
Toni Petrina <toni@toni.com>
2017-01-27 11:22:37 +0100

    Added clickable rows to my gists page

--- NuGist.Web/wwwroot/js/site.js
+++ NuGist.Web/wwwroot/js/site.js
@@ -1,1 +1,10 @@
 ﻿// Write your Javascript code.
+$('.table tr').each(function (index, element) {
+    var href = $(this).data('href');
+    if (href)
+        $(this).children('td:not(:last)')
+            .click(function (e) {
+                e.preventDefault();
+                window.document.location = href;
+            });
+});
